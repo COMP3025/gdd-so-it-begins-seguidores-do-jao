29e62a314c9dde4230e231dad228782acff88765
joao vitor carvalho <joao.carvalho@edu.unifil.br>
2023-11-28 19:02:40 -0300

    ponto de termino da fase

--- jogo-grupo-do-jao/scripts/goal.gd
+++ jogo-grupo-do-jao/scripts/goal.gd
@@ -1,4 +1,10 @@
 extends Area2D
 
+@onready var transition = $"../transition"
+@export var next_level : String = ""
+
 func _on_body_entered(body):
-	pass # Replace with function body.
+	if body.name == "player" and !next_level == "":
+		transition.change_scene(next_level)
+	else:
+		print('nem uma cena')
